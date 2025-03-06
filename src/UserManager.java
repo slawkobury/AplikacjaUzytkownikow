2d69266a668ed18b2d15bc23b3ceb76983719f37
slawko <slawkobury@gmail.com>
2025-03-06 14:14:28 +0100

    Creating UserManager Clasa UserManager uzupelniona

--- src/UserManager.java
+++ src/UserManager.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserManager {
+
+
+    private List<User> listaUzytkownikow1;
+
+    public UserManager() {
+        listaUzytkownikow1 = new ArrayList<>();
+    }
+
+    // Metoda dodająca użytkownika do listy
+    public void dodajUzytkownikow1(User user1) {
+        listaUzytkownikow1.add(user1);
+    }
+
+    // Metoda wyświetlająca wszystkich użytkowników
+    //uzytkownikMojaNowaNazwa nowy objekt classy  User.  Znika po zakonczeniu metody wyswietlUzytkownikow1)
+    public void wyswietlUzytkownikow1{
+        for (User uzytkownikMojaNowaNazwa : listaUzytkownikow1) {
+            System.out.println("Nazwa: " + uzytkownikMojaNowaNazwa.getUsername1() + ", Email: " + uzytkownikMojaNowaNazwa.getEmail());
+        }
+    }
 }
